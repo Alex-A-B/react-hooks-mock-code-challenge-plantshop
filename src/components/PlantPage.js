dde8b6cfa73b6fa3324cb9004c4eb6ff0d9852eb
Alex-A-B <alexjamesbelton@gmail.com>
2021-08-03 11:55:31 +0100

    base challenges completed

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantArray, setPlantArray] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
+
+  useEffect( ()=> {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlantArray(data))
+  }, [])
+
+  function onAddPlant(newPlant) {
+    const updatedPlantArray = [...plantArray, newPlant]
+    setPlantArray(updatedPlantArray)
+  }
+
+  const displayPlants = plantArray.filter((plant) => plant.name.toLowerCase().includes(searchQuery.toLowerCase()))
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
+      <PlantList plantArray={displayPlants} />
     </main>
   );
 }
